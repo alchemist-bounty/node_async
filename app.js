7056fc142e55f89de6d9152757779bec207709c8
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-19 07:09:00 -0500

    add api

---
+++ app.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'; 
+
+async function getTotalGoals(team, year) {
+	// https://jsonmock.hackerrank.com/api/football_matches?year=<year>&team1=<team>&page=<page>
+	let url = "https://jsonmock.hackerrank.com/api/football_matches?";
+	const response= await fetch(url+"year="+year+"&team1="+team+"&page=1");
+	const data=await response.json();
+
+
+	console.log(data);
+}
+
+getTotalGoals('Barcelona', 2011);
